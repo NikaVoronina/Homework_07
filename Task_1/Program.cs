92bfd9c4f707a3ee9dd22cef66d5ff1d93fcfc4a
Ника Воронина <nika_voronina_95@mail.ru>
2022-08-29 19:26:08 +0300

    Task_1 added

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -7,3 +7,35 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+Console.Write("Введите количество строк массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int b = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[a, b];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
